c39a16a84cad4fbdc054a4670396f072d63e20c2
AcidicDream <37349146+AcidicDream@users.noreply.github.com>
2018-03-20 15:41:00 +0100

    Add files via upload

---
+++ utils/Visualize.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def diff(img, img1):  # returns just the difference of the two images
+    return cv2.absdiff(img, img1)
+
+
+def imshow(img,text=None,should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic',fontweight='bold',
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+def show_plot(iteration,loss):
+    plt.plot(iteration,loss)
+    plt.show()
+
+
+
+
+def to_img(x):
+    x = 0.5 * (x + 1)
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
---
+++ utils/preProcess.py
@@ -0,0 +1,20 @@
+import cv2
+from PIL import Image, ImageChops
+import matplotlib.pyplot as plt
+import os
+
+def diff(img, img1):  # returns just the difference of the two images
+    return cv2.absdiff(img, img1)
+
+
+img1 = Image.open('/home/jasper/Documents/BP_Jasp/data/pg/all/T0412_S008_U014/444-1.jpg' )
+img2 = Image.open('/home/jasper/Documents/BP_Jasp/data/pg/all/T0412_S008_U014/444-2300.jpg' )
+
+
+img1=img1.convert('LA')
+img2=img2.convert('LA')
+diff = ImageChops.subtract(img1, img2)
+diff = diff.point(lambda i: i * 5)
+w ,h = diff.size
+area = (9,70,115,229)
+diff.crop(area).show()
